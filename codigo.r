2c34e93e469b615894768618ae974a8431e8ad8b
Robson Wilson Silva Pessoa <robsonpessoa2007@gmail.com>
2022-07-18 00:31:42 -0300

    passo2 leitura

--- codigo.r
+++ codigo.r
@@ -2,4 +2,6 @@
 
 # passo - 01: Instalar "readr" - terminal R
 install.packages("readr")
- 
+# passo - 02: Leitura do arquivo
+dados <- read.csv("C:/Users/robso/OneDrive/Documentos/R/ed_number_ssa/data/Quant_Geral_Alunos_Escola_Regional_2022.csv", 
+    sep = ";")
