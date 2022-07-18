9875bc3056b6bd4c8a35ee2d219a6315c0bd0466
Robson Wilson Silva Pessoa <robsonpessoa2007@gmail.com>
2022-07-18 00:57:41 -0300

    remocao install e paso3

--- codigo.r
+++ codigo.r
@@ -1,7 +1,10 @@
 #Analise de dados de educacao
 
 # passo - 01: Instalar "readr" - terminal R
-install.packages("readr")
+#install.packages("readr")
 # passo - 02: Leitura do arquivo
 dados <- read.csv("C:/Users/robso/OneDrive/Documentos/R/ed_number_ssa/data/Quant_Geral_Alunos_Escola_Regional_2022.csv", 
     sep = ";")
+# passo - 03: Escolas com numero de alunos menor que 75
+library("dplyr")
+dados %>% filter(QUANTIDADE < 75)
